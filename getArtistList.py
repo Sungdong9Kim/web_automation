4afe6bbebee3fd1402eb6fb9a24ac1ddcd1e5a42
Sungdong9Kim <sungdong9.kim@gmail.com>
2021-03-14 23:20:19 +0900

    feat : add project for get artist list

---
+++ getArtistList.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://workey.codeit.kr/music")
+
+music_page = response.text
+
+soup = BeautifulSoup(music_page,'html.parser')
+
+print(soup.select(('ul.popular__order li')))
+
+popular_artists = []
+
+
+for tag in soup.select('ul.popular__order li'):
+    print(tag.get_text().strip())
+    print(list(tag.strings))
+    print(list(tag.stripped_strings))
+    print(list(tag.stripped_strings)[1])
+    popular_artists.append(list(tag.stripped_strings)[1])
+    print(popular_artists)
